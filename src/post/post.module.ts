1af8f6585426d3c9cbae0dd45b4a1b1e77dec43b
wallacedba <wallacedba@gmail.com>
2022-06-29 20:21:07 -0300

    feat(post): add postRepository as provider and export postService

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { PostRepository } from './repository/post.repository';
 import { PostService } from './services/post.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
-  providers: [PostService],
+  providers: [PostService, PostRepository],
+  exports: [PostService],
 })
 export class PostModule {}
