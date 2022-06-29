b744414ba9a4b9ea54337b4b7da394fe6712c063
wallacedba <wallacedba@gmail.com>
2022-06-29 18:49:21 -0300

    feat(app): add configModule and inject configService on TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,40 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HomeModule } from './home/home.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { join } from 'path';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [HomeModule, UserModule, PostModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+      envFilePath: '.env',
+      cache: true,
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        autoLoadEntities: true,
+        migrations: ['dist/migrations/*{.ts,.js}'],
+        cli: {
+          migrationsDir: 'migrations',
+        },
+      }),
+    }),
+
+    HomeModule,
+    UserModule,
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
