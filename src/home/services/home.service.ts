ea84ff9ca68ad359c322fba7baa2b68b3da96114
wallacedba <wallacedba@gmail.com>
2022-06-30 20:55:57 -0300

    feat(home): add method to find posts

--- src/home/services/home.service.ts
+++ src/home/services/home.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from '../../post/entities/post.entity';
+import { PostService } from '../../post/services/post.service';
+import { PaginationOptions } from '../../shared/interfaces/pagination.interface';
+import { PaginatedResponse } from '../../shared/utils/pagination.util';
+import { HomeFindRequestDto } from '../dto/home-find-request.dto';
 
 @Injectable()
-export class HomeService {}
+export class HomeService {
+  constructor(private readonly postService: PostService) {}
+
+  async findPosts(
+    homeFindResquestDto: HomeFindRequestDto,
+    pagination: PaginationOptions,
+  ): Promise<PaginatedResponse<Post[]>> {
+    if (homeFindResquestDto.onlyMyPosts) {
+      return await this.postService.findUserPosts(
+        homeFindResquestDto.userId,
+        pagination,
+      );
+    }
+
+    return await this.postService.findWithFilters({
+      ...homeFindResquestDto,
+      ...pagination,
+    });
+  }
+}
