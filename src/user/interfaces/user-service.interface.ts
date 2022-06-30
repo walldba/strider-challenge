cf2ef3c77c32fea7996c56fe928c1f6bb34aae7f
wallacedba <wallacedba@gmail.com>
2022-06-30 14:33:19 -0300

    refactor(user): adjust user service interface

--- src/user/interfaces/user-service.interface.ts
+++ src/user/interfaces/user-service.interface.ts
@@ -1,8 +1,19 @@
 import { PostCreateRequestDto } from '../../post/dto/post-create-request.dto';
+import { Post } from '../../post/entities/post.entity';
+import { PaginationOptions } from '../../shared/interfaces/pagination.interface';
+import { PaginatedResponse } from '../../shared/utils/pagination.util';
+import { User } from '../entities/user.entity';
 
 export interface IUserService {
+  findById(id: string): Promise<User>;
+
+  findPosts(
+    userId: string,
+    pagination: PaginationOptions,
+  ): Promise<PaginatedResponse<Post[]>>;
+
   createPost(
     userId: string,
     postCreateRequestDto: PostCreateRequestDto,
-  ): Promise<any>;
+  ): Promise<Post>;
 }
