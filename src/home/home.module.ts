6dba9320e0a1629ee5b31d9167b6a7093d33f30c
wallacedba <wallacedba@gmail.com>
2022-06-30 20:49:48 -0300

    feat(home): import PostModule and add HomeController

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PostModule } from '../post/post.module';
+import { HomeController } from './controllers/home.controller';
 import { HomeService } from './services/home.service';
 
 @Module({
-  providers: [HomeService]
+  imports: [PostModule],
+  providers: [HomeService],
+  controllers: [HomeController],
 })
 export class HomeModule {}
