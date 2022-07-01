22ffaee79ee3780dd463a3486f7fb5879ec489bd
wallacedba <wallacedba@gmail.com>
2022-06-30 21:17:37 -0300

    feat(app): add endpoint to get app status

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
+export interface AppStatus {
+  name: string;
+  uptime: number;
+  version: string;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  static readonly STARTED_AT = Date.now();
+
+  constructor() {}
+
+  public async getStatus(): Promise<AppStatus> {
+    const packageInfo = await import('../package.json');
+    return {
+      name: packageInfo.name,
+      version: packageInfo.version,
+      uptime: Date.now() - AppService.STARTED_AT,
+    };
   }
 }
